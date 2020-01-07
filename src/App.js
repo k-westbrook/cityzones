6888c1c6f5fb558ea582d983b0d7e5c699020cb4
k-westbrook <kcheriewestbrook@gmail.com>
2020-01-06 22:38:17 -0800

    updated routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Routes from './Routes';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>cityzones</h1>
-      </header>
+    <div>
+      <Router>
+        <Routes />
+      </Router>
     </div>
   );
 }
---
+++ src/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function HomePage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>cityzones</h1>
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;

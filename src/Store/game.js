81cc00f56717f45db0f744e84cd6c2a2c61f1b97
k-westbrook <kcheriewestbrook@gmail.com>
2020-01-08 10:23:58 -0800

    updated reducer

---
+++ src/Store/game.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import history from '../history'
+
+/**
+ * ACTION TYPES
+ */
+const SET_NAMES = 'SET_NAMES';
+
+
+/**
+ * INITIAL STATE
+ */
+const gameObject = {
+  grid: [],
+  mayorName: '',
+  cityName: ''
+};
+
+
+/**
+ * ACTION CREATORS
+ */
+
+const setNames = (mayorName, cityName) => ({ type: SET_NAMES, mayorName, cityName })
+
+
+
+/**
+ * THUNK CREATORS
+ *///SPACE HOLDEr
+
+
+
+/**
+ * REDUCER
+ */
+export default function (state = gameObject, action) {
+  switch (action.type) {
+    case SET_NAMES:
+      return { ...gameObject, mayorName: action.mayorName, cityName: action.cityName }
+
+    default:
+      return state
+  }
+}

be1bbaa599b4ae48a9b32950ad34e858c06b2925
k-westbrook <kcheriewestbrook@gmail.com>
2020-01-08 21:51:17 -0800

    update account

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import Grid from './Grid';
+import { connect } from 'react-redux'
 
 
-function GameBoard() {
+function GameBoard(props) {
 
 
   return (
     <div>
       <h1> Gameboard</h1>
+      <p>Bank Account: ${props.bankTotal}</p>
       <Grid />
     </div>
   )
 }
 
-export default GameBoard;
+const mapState = (state) => {
+  return {
+    bankTotal: state.game.bankTotal
+
+  }
+}
+
+
+export default connect(mapState)(GameBoard);

43dd4bdd00660c6cc6d31f9da8fe512e4cb7bf9f
k-westbrook <kcheriewestbrook@gmail.com>
2020-01-08 20:48:16 -0800

    updated to button

--- src/LotMenu.js
+++ src/LotMenu.js
@@ -3,17 +3,22 @@ import React from 'react';
 
 
 function LotMenu(props) {
+
+  function handleClick(evt) {
+
+    console.log(evt.target.value)
+  }
   return (
     <div className='dropdown-lot-menu'>
       <div>
         <div>
           {(props.lotType === 'empty') ?
             <div className='build-lot-menu'>
-              <p>Residential</p>
-              <p>Commercial</p>
-              <p>School</p>
-              <p>Hospital</p>
-
+              <button onClick={handleClick} value='residential' >Residential</button>
+              <button onClick={handleClick}
+                value='commercial'>Commercial</button>
+              <button onClick={handleClick} value='hospital'>Hospital</button>
+              <button onClick={handleClick} value='school'>School</button>
             </div>
             :
             <div>
